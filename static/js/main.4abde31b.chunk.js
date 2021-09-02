1fd73eb44f1662e9a1cfdce112c9388f0d332c2d
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2021-09-02 18:05:18 +0200

    Updates

---
+++ static/js/main.4abde31b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback1"]=this["webpackJsonpgoit-react-hw-02-feedback1"]||[]).push([[0],[function(e,c){}],[[0,1]]]);
+//# sourceMappingURL=main.4abde31b.chunk.js.map
